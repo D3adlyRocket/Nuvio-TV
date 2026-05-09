580cdb1a610255d6007648fdcffaefacb18e1f1a
D3adlyRocket <31734254+D3adlyRocket@users.noreply.github.com>
2026-05-09 16:48:21 +0100

    Update DEFAULT_MAIN_URL to new domain

--- providers/4khdhub.js
+++ providers/4khdhub.js
@@ -75,7 +75,7 @@ var import_cheerio_without_node_native2 = __toESM(require("cheerio-without-node-
 
 // src/4KHDHub/http.js
 var DOMAINS_URL = "https://raw.githubusercontent.com/phisher98/TVVVV/refs/heads/main/domains.json";
-var DEFAULT_MAIN_URL = "https://4khdhub.dad";
+var DEFAULT_MAIN_URL = "https://4khdhub.link";
 var HEADERS = {
   "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
   "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
